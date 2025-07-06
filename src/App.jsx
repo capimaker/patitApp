b5ca0b2e59d214931d4be730165cd75ee4ad00fb
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-07-06 16:23:33 +0200

    register existosamente mergeado con develop

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Posts from "./components/Posts/Posts";
 
 
 function App() {
