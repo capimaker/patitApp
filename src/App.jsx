efa0313596a89c273b6da17aca979de0a7eae95f
capimaker <capimaker@gmail.com>
2025-07-06 04:00:01 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,20 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Posts from "./components/Posts/Posts";
 import Header from './components/Header/Header';
-import Login from "./components/Login/Login";
 import Profile from './components/Profile/Profile';
-
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Posts from "./components/Posts/Posts";
 import Login from "./components/Login/Login";
 
 
 function App() {
   return (
-    <div>
+    <>
     <BrowserRouter>
     <Header/>
-      <Routes>
-        
-        <Route path="/login" element={<Login/>} />
-        <Route path="/profile" element={<Profile/>} />
-      </Routes>
-    <>
-      <BrowserRouter>
         <Routes>
           <Route path="/posts" element={<Posts />} />
           <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
         </Routes>
       </BrowserRouter>
     </>
