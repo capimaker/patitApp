2f4341b566606707f89160b2b64f79898b7e5308
Beatriz-Valencia <beatriz.conchado.peiro@gmail.com>
2025-07-07 20:11:20 +0200

    arreglos estilos ok a falta de gati style

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput/SearchInput";
 const Search = () => {
   return (
     <>
-      <h1>Explora los mejores posts</h1>
+      <h1 style={{ marginTop: "70px" }}>Explora los mejores posts</h1>
       <div>
         <SearchInput />
         <Post />
